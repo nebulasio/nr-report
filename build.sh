8bb8bbbbc896dfb5d21ec4c2865a6d47e3a43d5a
Xuepeng.Fan <athrunarthur@gmail.com>
2018-05-24 14:06:33 +0800

    add ch

---
+++ build.sh
@@ -0,0 +1,5 @@
+cp common/common.tex ./ch
+#cd ./ch && xelatex main.tex && biber main && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+cd ./ch && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+
+cd ../
